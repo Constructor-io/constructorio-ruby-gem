490994c82719438c20920e599897d1d87e81adfa
Dan McCormick <dan@codeop.com>
2015-08-21 15:55:02 -0400

    add more API calls, version to 2.0

--- lib/constructorio/client.rb
+++ lib/constructorio/client.rb
@@ -3,32 +3,48 @@ require 'json'
 module ConstructorIO
   class Client
     def add(params)
-      call_api("post", params)
+      call_api("item", "post", params)
     end
 
     def remove(params)
-      call_api("delete", params)
+      call_api("item", "delete", params)
     end
 
     def modify(params)
-      call_api("put", params)
+      call_api("item", "put", params)
+    end
+
+    def track_search(params)
+      call_api("search", "post", params)
+    end
+
+    def track_click_through(params)
+      call_api("click_through", "post", params)
+    end
+
+    def track_conversion(params)
+      call_api("conversion", "post", params)
+    end
+
+    def verify
+      call_api("verify", "get")
     end
 
     private
 
-    def call_api(method, params)
+    def call_api(path, method, params)
       api_token = ConstructorIO.configuration.api_token
       api_url = ConstructorIO.configuration.api_url || "https://ac.constructor.io/"
       autocomplete_key = ConstructorIO.configuration.autocomplete_key
       @http_client ||= Faraday.new(url: api_url)
       @http_client.basic_auth(api_token, '')
 
-      send_request(method, @http_client, params, autocomplete_key)
+      send_request(path, method, @http_client, params, autocomplete_key)
     end
 
-    def send_request(method, http_client, params, autocomplete_key)
+    def send_request(path, method, http_client, params, autocomplete_key)
       response = http_client.send(method) do |request|
-        request.url "/v1/item?autocomplete_key=#{autocomplete_key}"
+        request.url "/v1/#{path}?autocomplete_key=#{autocomplete_key}"
         request.headers['Content-Type'] = 'application/json'
         request.body = params.to_json
       end
--- test/constructorio_test.rb
+++ test/constructorio_test.rb
@@ -4,7 +4,10 @@ class ConstructorIOTest < MiniTest::Test
   def test_add_record_calls_post
     c = ConstructorIO::Client.new
     c.expects(:call_api).with(
-      "post", { item_name: "power drill" })
+      "item",
+      "post",
+      { item_name: "power drill" }
+    )
 
     c.add( { item_name: "power drill" } )
   end
@@ -12,6 +15,7 @@ class ConstructorIOTest < MiniTest::Test
   def test_add_record_sends_request
     c = ConstructorIO::Client.new
     c.expects(:send_request).with(
+      "item",
       "post",
       instance_of(Faraday::Connection),
       { item_name: "power drill" },
@@ -24,7 +28,10 @@ class ConstructorIOTest < MiniTest::Test
   def test_remove_record_calls_delete
     c = ConstructorIO::Client.new
     c.expects(:call_api).with(
-      "delete", { item_name: "power drill" })
+      "item",
+      "delete",
+      { item_name: "power drill" }
+    )
 
     c.remove( { item_name: "power drill" } )
   end
@@ -32,7 +39,10 @@ class ConstructorIOTest < MiniTest::Test
   def test_modify_record_calls_put
     c = ConstructorIO::Client.new
     c.expects(:call_api).with(
-      "put", { item_name: "power drill" })
+      "item",
+      "put",
+      { item_name: "power drill" }
+    )
 
     c.modify( { item_name: "power drill" } )
   end
